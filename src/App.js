61cb8ab7de02b919478a8e69fb7242c9be246010
Dinesh Gautam <dineshgautam5252@gmail.com>
2024-01-16 12:05:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,446 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [info, setInfo] = useState(null);
+  const [days, setDays] = useState(null);
+  const originalDate = new Date();
+
+  const [todayDate, setToDayDate] = useState(originalDate);
+  useEffect(() => {
+    // get info from localStorage
+    const info = JSON.parse(localStorage.getItem("info"));
+    const days = JSON.parse(localStorage.getItem("days"));
+    console.log(info);
+    setInfo(info || {});
+    setDays(days ?? {});
+  }, []);
+
+  useEffect(() => {
+    // save info to local storage
+    if (!info) return;
+    localStorage.setItem("info", JSON.stringify(info));
+  }, [info]);
+
+  useEffect(() => {
+    // save info to local storage
+    if (!days) return;
+    localStorage.setItem("days", JSON.stringify(days));
+  }, [days]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    info !== null && (
+      <div className="App">
+        {!info?.className && <GetClassInfo info={info} setInfo={setInfo} />}
+        {(!info?.startDate || !info?.endDate) && (
+          <GetStartAndEndDate info={info} setInfo={setInfo} />
+        )}
+        {!info?.subjects && <GetSubjectNames info={info} setInfo={setInfo} />}
+
+        {isAllDataInserted(info) && (
+          <>
+            <GetTodayAttendance
+              info={info}
+              setInfo={setInfo}
+              days={days}
+              setDays={setDays}
+              todayDate={todayDate}
+              setToDayDate={setToDayDate}
+              originalDate={originalDate}
+            />
+            <Calendar
+              info={info}
+              days={days}
+              setDays={setDays}
+              todayDate={todayDate}
+              setToDayDate={setToDayDate}
+              originalDate={originalDate}
+            />
+          </>
+        )}
+      </div>
+    )
+  );
+}
+
+function isAllDataInserted(info) {
+  return info.className && info.startDate && info.endDate && info.subjects;
+}
+
+function GetClassInfo({ info, setInfo }) {
+  const [value, setValue] = useState(info?.className || "");
+  return (
+    <form onSubmit={(e) => setInfo({ ...info, className: value })}>
+      <label for="classNameInput">Class Name</label>
+      <input
+        id="classNameInput"
+        placeholder="1CE12"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function GetStartAndEndDate({ info, setInfo }) {
+  const [value, setValue] = useState({
+    startDate: info?.startDate || "",
+    endDate: info?.endDate || "",
+  });
+  return (
+    <form onSubmit={(e) => setInfo({ ...info, ...value })}>
+      <label for="startDateInput">Start Date</label>
+      <input
+        id="startDateInput"
+        type="date"
+        value={value.startDate}
+        onChange={(e) =>
+          setValue((prev) => ({
+            ...prev,
+            startDate: e.target.value,
+          }))
+        }
+      />
+      <label for="endDateInput">End Date</label>
+      <input
+        id="endDateInput"
+        type="date"
+        value={value.endDate}
+        onChange={(e) =>
+          setValue((prev) => ({ ...prev, endDate: e.target.value }))
+        }
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function GetSubjectNames({ info, setInfo }) {
+  const [value, setValue] = useState(
+    info?.subjects?.map((s) => s.name).join(",") || ""
+  );
+  return (
+    <form
+      onSubmit={(e) =>
+        setInfo({
+          ...info,
+          subjects: value
+            .split(",")
+            .map((s, index) => ({ name: s.trim(), id: index + 1 })),
+        })
+      }
+    >
+      <label for="subjectsInput">Class Name</label>
+      <input
+        id="subjectsInput"
+        placeholder="subject1,subject2,subject3"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function GetTodayAttendance({ info, days, setDays, todayDate, originalDate }) {
+  const todaysDate = todayDate.toDateString();
+  return (
+    <div className="attendance-table-container">
+      <div>
+        <span>Today's Date :</span>
+        <span className="bold">{originalDate.toDateString()}</span>
+      </div>
+      <div className="attendance-table">
+        <div className="attendance-table-header">
+          <span>Showing attendance: </span>
+          <span className="bold">{todaysDate}</span>
+          {/* <p>select subjects in which you were present</p> */}
+        </div>
+
+        <table cellSpacing="0">
+          <thead>
+            <tr>
+              <th>Subjects</th>
+              <th>Toady Attendance</th>
+              <th>Attended</th>
+              <th>Lectures</th>
+              <th>Percentage</th>
+            </tr>
+          </thead>
+          <tbody>
+            {info.subjects.map((subject) => {
+              return (
+                <tr key={subject.id}>
+                  <td>{subject.name}</td>
+                  <td style={{ display: "flex", gap: "1em" }}>
+                    <button
+                      style={
+                        days?.[todaysDate]?.[subject.id]?.present
+                          ? {
+                              backgroundColor: "lightgreen",
+                            }
+                          : {}
+                      }
+                      onClick={(e) =>
+                        setDays((prev) => ({
+                          ...prev,
+                          [todaysDate]: {
+                            ...prev[todaysDate],
+                            [subject.id]: {
+                              present:
+                                !prev?.[todaysDate]?.[subject.id]?.present,
+
+                              absent: false,
+                            },
+                          },
+                        }))
+                      }
+                    >
+                      Present
+                    </button>
+                    <button
+                      style={
+                        days?.[todaysDate]?.[subject.id]?.absent
+                          ? {
+                              backgroundColor: "red",
+                            }
+                          : {}
+                      }
+                      onClick={(e) =>
+                        setDays((prev) => ({
+                          ...prev,
+                          [todaysDate]: {
+                            ...prev[todaysDate],
+                            [subject.id]: {
+                              absent: !prev?.[todaysDate]?.[subject.id]?.absent,
+                              present: false,
+                            },
+                          },
+                        }))
+                      }
+                    >
+                      Absent
+                    </button>
+                  </td>
+                  <td>{getAttendedLecturesNumber(subject, days)}</td>
+                  <td>{getTotalLecturesNumber(subject, days)}</td>
+                  <td>
+                    {getAttendedLecturesPercentage(subject, days).toFixed(1)}%
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td />
+              <td style={{ display: "flex", gap: "1em" }}>
+                <button
+                  onClick={(e) =>
+                    setDays((prev) => ({
+                      ...prev,
+                      [todaysDate]: {
+                        ...prev[todaysDate],
+                        ...info.subjects.reduce((acc, subject) => {
+                          acc[subject.id] = {
+                            present: true,
+                            absent: false,
+                          };
+                          return acc;
+                        }, {}),
+                      },
+                    }))
+                  }
+                >
+                  All present
+                </button>
+                <button
+                  onClick={(e) =>
+                    setDays((prev) => ({
+                      ...prev,
+                      [todaysDate]: {
+                        ...prev[todaysDate],
+                        ...info.subjects.reduce((acc, subject) => {
+                          acc[subject.id] = {
+                            present: false,
+                            absent: true,
+                          };
+                          return acc;
+                        }, {}),
+                      },
+                    }))
+                  }
+                >
+                  All Absent
+                </button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
+}
+
+function getTotalLecturesNumber(subject, days) {
+  days = Object.values(days);
+  const totalLectures = days.map((day) => {
+    return day[subject.id]?.present || day[subject.id]?.absent ? 1 : 0;
+  });
+  return totalLectures.reduce((a, b) => a + b, 0);
+}
+function getAttendedLecturesNumber(subject, days) {
+  days = Object.values(days);
+  const totalLectures = days.map((day) => {
+    return day[subject.id]?.present ? 1 : 0;
+  });
+  return totalLectures.reduce((a, b) => a + b, 0);
+}
+function getAttendedLecturesPercentage(subject, days) {
+  const totalLectures = getTotalLecturesNumber(subject, days);
+  const attendedLectures = getAttendedLecturesNumber(subject, days);
+  return (attendedLectures / totalLectures) * 100;
+}
+
+function Calendar({ info, days, todayDate, setToDayDate, originalDate }) {
+  const noOfMonth = getNoOfMonth(info.startDate, info.endDate);
+  return (
+    <div>
+      {[...Array(noOfMonth)].map((_, monthNo) => {
+        const date = new Date(info.startDate);
+        date.setMonth(monthNo + 1);
+
+        const noOfDays = getNoOfDays(monthNo + 1, date.getFullYear());
+
+        return (
+          <div
+            key={monthNo}
+            style={{
+              display: "inline-block",
+              margin: "10px",
+              textAlign: "center",
+            }}
+          >
+            <h3 style={{ marginBottom: ".5em" }}>{getMonthName(monthNo)}</h3>
+            <table cellSpacing={2} className="calendar">
+              <thead>
+                <tr>
+                  <th>Mon</th>
+                  <th>Tue</th>
+                  <th>Wed</th>
+                  <th>Thu</th>
+                  <th>Fri</th>
+                  <th>Sat</th>
+                  <th>Sun</th>
+                </tr>
+              </thead>
+              <tbody>
+                {[...Array(Math.ceil(noOfDays / 7))].map((_, weekNo) => (
+                  <tr key={monthNo + weekNo}>
+                    {[...Array(7)].map((_, weekDayNo) => {
+                      const day = getDate(
+                        weekDayNo +
+                          1 +
+                          7 * weekNo -
+                          getMonthStartOffset(monthNo, date.getFullYear()),
+
+                        monthNo,
+                        date.getFullYear()
+                      );
+                      const currentDate = new Date(
+                        date.getFullYear(),
+                        monthNo,
+                        day
+                      ).toDateString();
+                      return (
+                        <td key={monthNo + weekNo + weekDayNo}>
+                          {day && (
+                            <button
+                              style={
+                                todayDate.toDateString() === currentDate
+                                  ? {
+                                      backgroundColor: "blue",
+                                      color: "white",
+                                    }
+                                  : currentDate === originalDate.toDateString()
+                                  ? {
+                                      backgroundColor: "white",
+                                    }
+                                  : {}
+                              }
+                              disabled={isCalendarButtonDisabled(
+                                day,
+                                monthNo,
+                                date.getFullYear(),
+                                info.startDate,
+                                info.endDate
+                              )}
+                              onClick={(e) =>
+                                setToDayDate(new Date(currentDate))
+                              }
+                            >
+                              {day}
+                            </button>
+                          )}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function isCalendarButtonDisabled(day, month, year, startDate, endDate) {
+  const date = new Date(year, month, day + 1);
+
+  return (
+    date < new Date(startDate) ||
+    date > new Date(endDate).setDate(new Date(endDate).getDate() + 1)
+  );
+}
+
+function getMonthName(month) {
+  return [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ][month];
+}
+
+function getMonthStartOffset(month, year) {
+  const date = new Date(year, month, 1);
+  return date.getDay() - 1;
+}
+
+function getDate(day, month, year) {
+  const date = new Date(year, month, day);
+  return date.getMonth() === month ? date.getDate() : null;
+}
+
+function getNoOfDays(month, year) {
+  return new Date(year, month + 1, 0).getDate();
+}
+
+function getNoOfMonth(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  return end.getMonth() - start.getMonth() + 1;
 }
 
 export default App;
