762c24659c85857b5c8fa5e3d921ae1084b72b9f
Dinesh-Gautam <dineshgautam5252@gmail.com>
2024-01-23 06:27:45 +0530

    improved code : timetableEditor

--- src/components/SetTimeTable.jsx
+++ src/components/SetTimeTable.jsx
@@ -1,230 +1,310 @@
 import { Button } from "@nextui-org/button";
 import { Card } from "@nextui-org/card";
 import { Input, ScrollShadow, Select, SelectItem } from "@nextui-org/react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { subjects } from "../config";
 import { getWeekName } from "../utils";
 import { FormButton } from "./Forms";
 import { useStateContext } from "../context/stateContext";
+import { Plus, X } from "react-feather";
+
+function convertDefaultSubjectsToSubjectsValues(info, noOfDays) {
+  const defaultSubjects = info?.timeTable || subjects;
+  const subjectsValues = {};
+
+  for (let day = 1; day <= noOfDays; day++) {
+    subjectsValues[day] = [];
+
+    for (const defaultSubject of defaultSubjects) {
+      if (
+        defaultSubject.lectures[day] &&
+        info.subjects.find((s) => s.id === defaultSubject.id)
+      ) {
+        for (const lecture of defaultSubject.lectures[day]) {
+          subjectsValues[day].push({
+            name: info.subjects.find((s) => s.id === defaultSubject.id).name,
+            id: info.subjects.find((s) => s.id === defaultSubject.id).id,
+            startTime: lecture.startTime,
+            endTime: lecture.endTime,
+          });
+        }
+      }
+    }
+  }
+  return subjectsValues;
+}
+function convertSubjectValuesToDefaultSubjectsValues(info, subjectsValues) {
+  const days = Object.keys(subjectsValues);
+
+  const res = info.subjects.map((sub) => {
+    const lectures = {};
+    for (const day of days) {
+      if (!subjectsValues[day]) continue;
+      if (!subjectsValues[day].find((s) => s.id === sub.id)) continue;
+      if (lectures[day] === undefined) lectures[day] = [];
+      for (const lecture of subjectsValues[day]) {
+        if (lecture.id === sub.id) {
+          lectures[day].push({
+            startTime: +lecture.startTime,
+            endTime: +lecture.endTime,
+          });
+        }
+      }
+    }
+    return {
+      name: sub.name,
+      id: sub.id,
+      lectures,
+    };
+  });
+  return res;
+}
 
 export function SetTimeTable() {
   const { info, setInfo } = useStateContext();
   const noOfDays = 5;
+
   const [subjectsValues, setSubjectsValues] = useState(
-    convertDefaultSubjectsToSubjectsValues()
+    convertDefaultSubjectsToSubjectsValues(info, noOfDays)
   );
-  function convertDefaultSubjectsToSubjectsValues() {
-    const defaultSubjects = info?.timeTable || subjects;
-    const subjectsValues = {};
 
-    for (let day = 1; day <= noOfDays; day++) {
-      subjectsValues[day] = [];
-
-      for (const defaultSubject of defaultSubjects) {
-        if (
-          defaultSubject.lectures[day] &&
-          info.subjects.find((s) => s.id === defaultSubject.id)
-        ) {
-          for (const lecture of defaultSubject.lectures[day]) {
-            subjectsValues[day].push({
-              name: info.subjects.find((s) => s.id === defaultSubject.id).name,
-              id: info.subjects.find((s) => s.id === defaultSubject.id).id,
-              startTime: lecture.startTime,
-              endTime: lecture.endTime,
-            });
-          }
-        }
-      }
-    }
-    return subjectsValues;
-  }
-  function convertSubjectValuesToDefaultSubjectsValues() {
-    const days = Object.keys(subjectsValues);
-
-    const res = info.subjects.map((sub) => {
-      const lectures = {};
-      for (const day of days) {
-        if (!subjectsValues[day]) continue;
-        if (!subjectsValues[day].find((s) => s.id === sub.id)) continue;
-        if (lectures[day] === undefined) lectures[day] = [];
-        for (const lecture of subjectsValues[day]) {
-          if (lecture.id === sub.id) {
-            lectures[day].push({
-              startTime: +lecture.startTime,
-              endTime: +lecture.endTime,
-            });
-          }
-        }
-      }
-      return {
-        name: sub.name,
-        id: sub.id,
-        lectures,
-      };
-    });
-    return res;
-  }
   function onChangeHandler(value, day, index, key) {
     setSubjectsValues((prev) => ({
       ...prev,
-      [day]: [
-        ...prev[day].map((s, i) => (i === index ? { ...s, [key]: value } : s)),
-      ],
+      [day]: prev[day].map((s, i) =>
+        i === index ? { ...s, [key]: value } : s
+      ),
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setInfo((prev) => ({
+      ...prev,
+      timeTable: convertSubjectValuesToDefaultSubjectsValues(
+        info,
+        subjectsValues
+      ),
     }));
   }
 
   return (
     <Card className="p-4 w-full mb-20">
-      <form
-        className="max-w-full w-full"
-        onSubmit={(e) => {
-          e.preventDefault();
-          setInfo((prev) => ({
-            ...prev,
-            timeTable: convertSubjectValuesToDefaultSubjectsValues(),
-          }));
-        }}
-      >
+      <form className="max-w-full w-full" onSubmit={handleSubmit}>
         {Array.from({ length: noOfDays }).map((_, day) => (
-          <div className="w-full max-w-full mb-2" key={day + 1}>
-            <span className="font-bold text-sm p-2">
-              {getWeekName(day + 1)}
-            </span>
-            {/* <div className="flex flex-col gap-2 items-start md:items-center md:flex-row"> */}
-            <ScrollShadow
-              orientation="horizontal"
-              className=" flex-1 flex flex-row gap-2 p-2 bg-default-100 rounded-2xl shadow-inner shadow-md overflow-auto items-center"
-            >
-              {subjectsValues[day + 1] &&
-                subjectsValues[day + 1].map((subject, index) => (
-                  <div
-                    key={subject.id + index + day}
-                    className="flex flex-col gap-1 bg-default-50 p-1 rounded-xl shadow-small focus-within:border-l-3"
-                    style={{
-                      minWidth: 200,
-                      // minWidth:
-                      //   100 * ((subject.endTime - subject.startTime) * 2),
-                      borderColor: "hsl(var(--nextui-danger))",
-                    }}
-                  >
-                    <div className="flex flex-row gap-1 flex-1 ">
-                      <Select
-                        variant="flat"
-                        size="sm"
-                        aria-label="Subject"
-                        id="subjectsInput"
-                        placeholder={"Subject " + (index + 1)}
-                        selectedKeys={[subjectsValues[day + 1][index].name]}
-                        onChange={(e) => {
-                          onChangeHandler(
-                            e.target.value,
-                            day + 1,
-                            index,
-                            "name"
-                          );
-                          onChangeHandler(
-                            info.subjects.find((s) => s.name === e.target.value)
-                              .id,
-                            day + 1,
-                            index,
-                            "id"
-                          );
-                        }}
-                      >
-                        {info?.subjects?.map((s) => (
-                          <SelectItem key={s.name} value={s.name}>
-                            {s.name}
-                          </SelectItem>
-                        ))}
-                      </Select>
-                      <Button
-                        variant="light"
-                        isIconOnly
-                        onClick={(e) => {
-                          e.preventDefault();
-                          setSubjectsValues((prev) => ({
-                            ...prev,
-                            [day + 1]: [
-                              ...prev[day + 1].filter((s, i) => i !== index),
-                            ],
-                          }));
-                        }}
-                      >
-                        <svg
-                          viewBox="0 0 24 24"
-                          width="12"
-                          height="12"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          fill="none"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="css-i6dzq1"
-                        >
-                          <line x1="18" y1="6" x2="6" y2="18"></line>
-                          <line x1="6" y1="6" x2="18" y2="18"></line>
-                        </svg>
-                      </Button>
-                    </div>
-
-                    <div className="flex flex-row gap-1">
-                      <Input
-                        size="sm"
-                        type={"number"}
-                        min={8}
-                        max={17}
-                        placeholder="Start Time"
-                        value={subjectsValues[day + 1][index].startTime}
-                        onChange={(e) =>
-                          onChangeHandler(
-                            e.target.value,
-                            day + 1,
-                            index,
-                            "startTime"
-                          )
-                        }
-                      />
-                      <Input
-                        size="sm"
-                        type={"number"}
-                        min={8}
-                        max={17}
-                        placeholder="End Time"
-                        value={subjectsValues[day + 1][index].endTime}
-                        onChange={(e) =>
-                          onChangeHandler(
-                            e.target.value,
-                            day + 1,
-                            index,
-                            "endTime"
-                          )
-                        }
-                      />
-                    </div>
-                  </div>
-                ))}
-
-              <Button
-                variant="flat"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setSubjectsValues((prev) => ({
-                    ...prev,
-                    [day + 1]: [
-                      ...(prev[day + 1] || []),
-                      { name: "", startTime: "", endTime: "" },
-                    ],
-                  }));
-                }}
-              >
-                Add
-              </Button>
-            </ScrollShadow>
-          </div>
-          // </div>
+          <DayKey
+            key={day + 1}
+            day={day + 1}
+            subjectsValues={subjectsValues}
+            onChangeHandler={onChangeHandler}
+            setSubjectsValues={setSubjectsValues}
+          />
         ))}
+
         <FormButton type="submit">Submit</FormButton>
       </form>
     </Card>
+  );
+}
+
+function DayKey({ day, subjectsValues, onChangeHandler, setSubjectsValues }) {
+  return (
+    <div className="w-full max-w-full mb-2">
+      <DayLabel day={day} />
+
+      <SubjectsList
+        day={day}
+        subjectsValues={subjectsValues}
+        onChangeHandler={onChangeHandler}
+        setSubjectsValues={setSubjectsValues}
+      />
+    </div>
+  );
+}
+
+function DayLabel({ day }) {
+  return <span className="font-bold text-sm p-2">{getWeekName(day)}</span>;
+}
+
+function SubjectsList({
+  day,
+  subjectsValues,
+  onChangeHandler,
+  setSubjectsValues,
+}) {
+  return (
+    <ScrollShadow
+      orientation="horizontal"
+      className="flex-1 flex flex-row gap-2 p-2 bg-default-100 rounded-2xl shadow-inner shadow-md overflow-auto items-center"
+    >
+      {subjectsValues[day] &&
+        subjectsValues[day].map((subject, index) => (
+          <SubjectKey
+            key={`${subject.id}-${index}-${day}`}
+            day={day}
+            index={index}
+            subject={subject}
+            onChangeHandler={onChangeHandler}
+            setSubjectsValues={setSubjectsValues}
+          />
+        ))}
+
+      <AddSubjectButton day={day} setSubjectsValues={setSubjectsValues} />
+    </ScrollShadow>
+  );
+}
+
+function AddSubjectButton({ day, setSubjectsValues }) {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <Button
+        isIconOnly
+        size="lg"
+        variant="flat"
+        onClick={(e) => {
+          setSubjectsValues((prev) => ({
+            ...prev,
+            [day]: prev[day].concat({
+              name: "",
+              id: "",
+              startTime: "",
+              endTime: "",
+            }),
+          }));
+        }}
+      >
+        <Plus />
+      </Button>
+    </div>
+  );
+}
+
+function SubjectKey({
+  day,
+  index,
+  subject,
+  onChangeHandler,
+  setSubjectsValues,
+}) {
+  const { info } = useStateContext();
+
+  return (
+    <div
+      className="flex flex-col gap-1 bg-default-50 p-1 rounded-xl shadow-small focus-within:border-l-3"
+      style={{
+        width: 200,
+        minWidth: 200,
+        borderColor: "hsl(var(--nextui-danger))",
+      }}
+    >
+      <SubjectNameInput
+        day={day}
+        index={index}
+        subjectName={subject.name}
+        subjects={info?.subjects}
+        setSubjectsValues={setSubjectsValues}
+        onChangeHandler={onChangeHandler}
+      />
+
+      <StartTimeEndTimeInputs
+        day={day}
+        index={index}
+        startTime={subject.startTime}
+        endTime={subject.endTime}
+        onChangeHandler={onChangeHandler}
+      />
+    </div>
+  );
+}
+
+function StartTimeEndTimeInputs({
+  day,
+  index,
+  startTime,
+  endTime,
+  onChangeHandler,
+}) {
+  const minStartTime = 8;
+  const maxEndTime = 22;
+  return (
+    <div className="flex flex-row gap-2">
+      <Input
+        type="number"
+        size="sm"
+        aria-label="Start Time"
+        id="startTimeInput"
+        placeholder={"Start Time " + (index + 1)}
+        value={startTime}
+        min={minStartTime}
+        max={maxEndTime}
+        onChange={(e) =>
+          onChangeHandler(e.target.value, day, index, "startTime")
+        }
+      />
+      <Input
+        type="number"
+        size="sm"
+        aria-label="End Time"
+        id="endTimeInput"
+        placeholder={"End Time " + (index + 1)}
+        value={endTime}
+        min={minStartTime}
+        max={maxEndTime}
+        onChange={(e) => onChangeHandler(e.target.value, day, index, "endTime")}
+      />
+    </div>
+  );
+}
+
+function SubjectNameInput({
+  day,
+  index,
+  subjectName,
+  subjects,
+  setSubjectsValues,
+  onChangeHandler,
+}) {
+  return (
+    <div className="flex flex-row gap-2">
+      <Select
+        variant="flat"
+        size="sm"
+        aria-label="Subject"
+        id="subjectsInput"
+        placeholder={"Subject " + (index + 1)}
+        selectedKeys={[subjectName]}
+        onChange={(e) => onChangeHandler(e.target.value, day, index, "name")}
+      >
+        {subjects?.map((s) => (
+          <SelectItem key={s.name} value={s.name}>
+            {s.name}
+          </SelectItem>
+        ))}
+      </Select>
+      <RemoveSubjectButton
+        day={day}
+        index={index}
+        setSubjectsValues={setSubjectsValues}
+      />
+    </div>
+  );
+}
+
+function RemoveSubjectButton({ day, index, setSubjectsValues }) {
+  return (
+    <Button
+      variant="light"
+      size="sm"
+      isIconOnly
+      onClick={(e) => {
+        setSubjectsValues((prev) => ({
+          ...prev,
+          [day]: prev[day].filter((s, i) => i !== index),
+        }));
+      }}
+    >
+      <X size="1.2em" opacity="0.5" />
+    </Button>
   );
 }
