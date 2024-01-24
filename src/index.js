e991f6efde649106f8be2cacda1a36e2f4c15cf6
Dinesh Gautam <dineshgautam5252@gmail.com>
2024-01-24 07:00:06 +0530

    fixed eslint warning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import { NextUIProvider } from "@nextui-org/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { StateProvider, useStateContext } from "./context/stateContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
