bd042c06eab276a92bf0769c8bd846625c258c7b
Dinesh Gautam <dineshgautam5252@gmail.com>
2024-03-23 17:42:55 +0530

    remove commend

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -136,10 +136,6 @@ function CalenderButton({ day, monthNo, date, currentDate }) {
         !isCurrentDateSelected()
           ? {
               backgroundColor,
-              // color:
-              //   originalDate.toDateString() === currentDate
-              //     ? `hsl(var(--nextui-default-300)`
-              //     : "white",
             }
           : {}
       }
